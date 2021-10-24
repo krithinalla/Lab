b9e8cd9d1e34070b0d490ea69d02fda8f91ba2ed
Krithi Nalla <krithi.nalla94@gmail.com>
2021-10-23 20:44:37 -0400

    pushing all

---
+++ Week 5(1)/svg.js
@@ -0,0 +1,6 @@
+document.getElementById("change").addEventListener("click",colors)
+let piggy = document.getElementById("change")
+
+function colors(){
+    piggy.setAttribute("style", "hover: rgb(0,0,0)");
+}
