056995e708e98e359a3253a43992db3df6230362
Krithi Nalla <krithi.nalla94@gmail.com>
2021-12-01 13:04:25 -0500

    rlx updt

--- circlr-gh-pages/spin.js
+++ circlr-gh-pages/spin.js
@@ -1,17 +1,2 @@
-$.getScript('index.js', function()
-{
-    window.onscroll = myScroll;
-    var counter = 0; // Global Variable
-    function myScroll(){
-       var val = document.getElementById("turn");
-       val.innerHTML = 'pageYOffset = ' + window.pageYOffset;
-       if(counter == 0){ // if counter is 1, it will not execute
-         if(window.pageYOffset > 300){
-            camera.play();
-            counter++; // increment the counter by 1, new value = 1
-         }
-       }
-      }
-});
 
 
